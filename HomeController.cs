583d971656c89286dd882ddd9770f9cb1e6f35ed
Siddsrk45 <67180593+Siddsrk45@users.noreply.github.com>
2022-06-19 11:51:16 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelPractice.Models;
+
+namespace ModelPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var data = GetEmployee();
+            Employee emp = new Employee()
+            {
+                Id = 1,
+                Address = "badlapur",
+                Name = "Siddhesh"
+            };
+            return View(emp);
+        }
+
+        //private Employee GetEmployee()
+        //{
+        //    return new Employee()
+        //    {
+        //        Id = 1,
+        //        Address = "India",
+        //        Name = "Siddhesh"
+        //    };
+        //}
+    }
+}
